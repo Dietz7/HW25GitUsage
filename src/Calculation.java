46ceee5d7cbb1172be3249739decc26fbb339872
Your Name <you@example.com>
2024-04-30 20:49:48 +0200

    update 1

---
+++ src/Calculation.java
@@ -0,0 +1,20 @@
+
+
+public class Calculation {
+    public static void main(String[] args) {
+        int targetNumber = 9 * 1915;
+        int result = sumUpTo(targetNumber);
+        System.out.println("sumUpTo(" + targetNumber + ") = " + result);
+
+    }
+
+    public static int sumUpTo(int targetNumber) {
+        int result = 0;
+
+        for (int i = 1; i<= targetNumber; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+}
